9f6a05b2a6838f702d05196972299d737a98cf60
manav-singhal12 <manavsinghal165@gmail.com>
2024-06-17 01:42:35 +0530

    ds

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='bg bg-purple-400 flex justify-between p-3 '>
-        <h1 className='font-bold'>PassMaps</h1>
+        <h1 className='font-bold text-2xl mx-10'>PassMaps</h1>
         <ul className='flex gap-6'>
-            <li className='hover:font-bold cursor-pointer'>Home</li>
-            <li className='hover:font-bold cursor-pointer'>About</li>
-            <li className='hover:font-bold cursor-pointer'>Contact Us</li>
+            Created with ♥ by Manav Singhal
         </ul>
     </nav>  
     )
